298ae0888a46a22b7f1fd388a40900957ca58f6a
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-05-24 00:08:16 -0700

    add worker

---
+++ scripts/handle_service_worker.js
@@ -0,0 +1,36 @@
+const listenToServiceWorkerUpdates = registration => {
+    registration.addEventListener('updatefound', () => {
+        registration.installing.addEventListener('statechange', event => {
+            switch (event.target.state) {
+                default: {
+                }
+                case 'installed': {
+                    console.log('service worker installed.');
+                }
+                case 'activated': {
+                    console.log('service worker activated.');
+                }
+            }
+        });
+    });
+};
+
+(function() {
+    if (!('serviceWorker' in navigator)) {
+        return;
+    }
+
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then(listenToServiceWorkerUpdates)
+            .catch(error =>
+                console.error(
+                    'Something bad happened in the service worker 🔥',
+                    {
+                        error
+                    }
+                )
+            );
+    });
+})();
