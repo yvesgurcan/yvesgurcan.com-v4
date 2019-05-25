338eede9c02c8b428bd5fd3a9199c2f59a14a902
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-05-24 23:44:50 -0700

    display event data

--- scripts/handle_github_activity.js
+++ scripts/handle_github_activity.js
@@ -1,3 +1,13 @@
+const handleRepoUrl = async event => {
+    const repoUrl = await event.getRepoUrl();
+    const repoElem = document.getElementById(`event-repo${event.id}`);
+    if (repoElem) {
+        repoElem.setAttribute('href', repoUrl);
+        repoElem.setAttribute('target', '_blank');
+        repoElem.setAttribute('rel', 'noopener');
+    }
+};
+
 (async function() {
     const EVENT_LIST_ELEMENT_ID = 'social-wall';
     const EVENT_CLASS = 'event';
@@ -18,27 +28,30 @@
 
         publicActivity.forEach((rawEvent, index) => {
             const event = new GitHubEvent(rawEvent, index);
-            const eventImage = document.createElement('img');
-
-            if (event.image) {
-                // eventImage.src = `assets/achievements/${achievement.image}.png`;
-            }
 
             const eventDetails = document.createElement('div');
             eventDetails.id = `event${event.id}`;
 
             const eventName = document.createElement('div');
+            eventName.classList = 'event-title';
             eventName.innerHTML = event.title;
             eventDetails.append(eventName);
 
-            const eventDescription = document.createElement('div');
-            // eventDescription.innerHTML = event.description;
-            eventDetails.append(eventDescription);
+            const eventRepo = document.createElement('a');
+            eventRepo.id = `event-repo${event.id}`;
+            eventRepo.innerHTML = event.repo.name;
+            eventDetails.append(eventRepo);
+
+            handleRepoUrl(event);
+
+            const eventTime = document.createElement('div');
+            eventTime.classList = 'event-time';
+            eventTime.innerHTML = event.timeFromNow;
+            eventDetails.append(eventTime);
 
             const eventItem = document.createElement('li');
             eventItem.className = EVENT_CLASS;
 
-            eventItem.append(eventImage);
             eventItem.append(eventDetails);
 
             eventList.append(eventItem);
